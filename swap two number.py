d62f5a284519af2a18719107972c7c835f7704df
Vinayakumara S S <vinaykumarss904@gmail.com>
2023-07-25 18:21:33 +0530

    Added new file

---
+++ swap two number.py
@@ -0,0 +1,7 @@
+def swap(num1,num2):
+    num1,num2=num2,num1
+    print(f"first number is {num1}")
+    print(f"second number is {num2}")
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+swap(num1,num2)
