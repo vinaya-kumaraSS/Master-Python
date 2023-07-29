e2e0540517296343c02c93b58e8ed803085928ed
Vinayakumara S S <vinaykumarss904@gmail.com>
2023-07-30 00:57:08 +0530

    Added new file

---
+++ Square and Cube of the number using lambda function.py
@@ -0,0 +1,5 @@
+cube= lambda num1:num1*num1*num1
+square=lambda num1:num1*num1
+num1=int(input("enter the number:"))
+print(f"square of the given number is {square(num1)}")
+print(f"cube of the given number is {cube(num1)}")
